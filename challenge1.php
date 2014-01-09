78bd90a46194bb1af6c70fa9c99407d1d571904c
Joe Ashcraft <joeashcraft@gmail.com>
2014-01-09 03:02:39 -0600

    print_r

--- challenge1.php
+++ challenge1.php
@@ -4,7 +4,7 @@ require 'vendor/autoload.php';
 use OpenCloud\Rackspace;
 
 $inifile = parse_ini_file("credentials.ini");
-
 $client = new Rackspace(Rackspace::US_IDENTITY_ENDPOINT, $inifile);
 
+print_r($client);
 ?>
