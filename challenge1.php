8eb48503f9e7a049d72f6c169138b876e5a2c13c
Joe Ashcraft <joeashcraft@gmail.com>
2014-01-09 03:22:48 -0600

    got image id

--- challenge1.php
+++ challenge1.php
@@ -6,17 +6,17 @@ use OpenCloud\Rackspace;
 $inifile = parse_ini_file("credentials.ini");
 $client = new Rackspace(Rackspace::US_IDENTITY_ENDPOINT, $inifile);
 
-var_dump($client);
+//var_dump($client);
 
 $compute = $client->computeService('cloudServersOpenStack', 'IAD');
 
 $images = $compute->imageList();
 while ($image = $images->next()){
-  if (strpos($image->name, 'CentOS') !== false) {
+  if (strpos($image->name, 'CentOS 6.4') !== false) {
     $centos64 = $image;
     break;
   }
 }
-
+print_r($centos64);
 //$centos64 = $compute->image('f70ed7c7-b42e-4d77-83d8-40fa29825b85');
 ?>
