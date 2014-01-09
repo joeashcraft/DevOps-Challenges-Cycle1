a9eff462a8b91d2d6844c26a24895b849746521d
Joe Ashcraft <joeashcraft@gmail.com>
2014-01-09 03:01:07 -0600

    template!

--- challenge1.php
+++ challenge1.php
@@ -1,6 +1,10 @@
 <?php
 
 require 'vendor/autoload.php';
+use OpenCloud\Rackspace;
 
+$inifile = parse_ini_file("credentials.ini");
+
+$client = new Rackspace(Rackspace::US_IDENTITY_ENDPOINT, $inifile);
 
 ?>
