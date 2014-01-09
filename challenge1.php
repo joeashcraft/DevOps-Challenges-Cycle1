c3c8e6b4994b32a37289f3027cc5c2475afb061f
Joe Ashcraft <joeashcraft@gmail.com>
2014-01-09 03:35:46 -0600

    static image id, flavor

--- challenge1.php
+++ challenge1.php
@@ -2,21 +2,30 @@
 
 require 'vendor/autoload.php';
 use OpenCloud\Rackspace;
+use OpenCloud\Compute\Constats\Network;
 
 $inifile = parse_ini_file("credentials.ini");
 $client = new Rackspace(Rackspace::US_IDENTITY_ENDPOINT, $inifile);
 
-//var_dump($client);
-
 $compute = $client->computeService('cloudServersOpenStack', 'IAD');
+print_r($computer->server());
 
-$images = $compute->imageList();
-while ($image = $images->next()){
-  if (strpos($image->name, 'CentOS 6.4') !== false) {
-    $centos64 = $image;
-    break;
-  }
-}
-print_r($centos64);
-//$centos64 = $compute->image('f70ed7c7-b42e-4d77-83d8-40fa29825b85');
+$centos64 = $compute->image('f70ed7c7-b42e-4d77-83d8-40fa29825b85');
+$halfGigFlavor = $compute->flavor('2');
+
+$server = $compute->server();
+
+try {
+  $response = $server->create(array(
+    'name'      =>  'API Challenge 1',
+    'image'     =>  $centos64,
+    'flavor'    =>  $halfGiGFlavor,
+    'networks'  =>  array(
+      $compute->network(Network::RAX_PUBLIC),
+      $compute->network(Network::RAX_PRIVATE)
+    )
+  ));
+} catch (\Guzzle\Http\Exception\BadResponseException)
+
+
 ?>
